413dbe3d5505e14c9a6c82583fd515b14192a901
Devin Connolly <dconnolly@triumf.ca>
2017-03-08 18:11:29 -0800

    building Kinematics class.

--- include/Kinematics.hxx
+++ include/Kinematics.hxx
@@ -23,12 +23,16 @@ namespace sonik {
 class Kinematics : public TObject {
 public:
   Kinematics();
-  Kinematics(Int_t Z_t, Int_t A_t, Int_t Z_b, Int_t A_b, Double_t E_b, Double_t theta);
-  Kinematics(const char Symbol_t, const char Symbol_b, Double_t E_b);
-  void reset();
+  Kinematics(Int_t Z_t, Int_t A_t, Int_t Z_b, Int_t A_b, Double_t T_b, Double_t theta);
+  Kinematics(const char Symbol_t, const char Symbol_b, Double_t T_b);
+
 
 public: // Data members
-  struct RxnPair {
+  // contains particle data for a scattering reaction
+  struct Rxn_t {         // data structure for elastic scattering reaction
+    Double_t fT_b;           // of non-identical particles.
+    Double_t fVelSquare;
+    Double_t fTheta_Lab;
     char fSymbol_b[3];
     char fSymbol_t[3];
     Int_t fA_b;
@@ -41,35 +45,52 @@ public: // Data members
     Double_t fm_t;
     Double_t fm_ej;
     Double_t fm_rec;
-    Double_t fE_b;
     Double_t fGammaLab;
     Double_t fBetaLab;
-    Double_t fminvariant;
+    Double_t fGammaCM;
+    Double_t fBetaCM;
+    Double_t fInvariantM;
     Double_t fRapidity;
-    Double_t fTheta_CM;
+    Double_t fThetaCM_ej;
+    Double_t fThetaCM_rec;
+    Double_t fp_ej;
+    Double_t fp_rec;
     Double_t fT_ej;
     Double_t fT_rec;
   };
 
-public: // methods
-  Double_t GammaLab(Double_t m_b, Double_t E_b);
-  Double_t BetaLab(Double_t gamma_lab);
-  Double_t E_CM(Double_t S, Double_t m_b, Double_t m_t);
-  Double_t P_CM(Double_t m_b, Double_t m_t, Double_t S);
-  Double_t Rapidity(Double_t p_CM, Double_t m_t);
-  Double_t T_ej(Double_t p_CM, Double_t rapidity, Double_t theta_CM, Double_t m_ej);
-  Double_t T_rec(Double_t p_CM, Double_t rapidity, Double_t theta_CM, Double_t m_rec);
-  Double_t Theta_ej();
-  Double_t Theta_rec();
-  Double_t MaxTheta_ej(Double_t p_CM, Double_t rapidity, Double_t m_ej);
-  Double_t MaxTheta_rec(Double_t p_CM, Double_t rapidity, Double_t m_rec);
 
-private:
+public: // public member functions
+  Double_t BetaCM(Double_t rapidity) { return tanh(rapidity); }
+  Double_t BetaLab(Double_t gamma_lab) { return sqrt(1 - 1 / pow(gamma_lab,2) ); }
+  Double_t E_CM(Double_t S, Double_t m_b, Double_t m_t) { return sqrt(S) - (m_b + m_t); }
+  Double_t GammaCM(Double_t rapidity) { return cosh(rapidity); }
+  Double_t GammaLab(Double_t m_b, Double_t T_b) { return E_b / M_b + 1; }
+  Double_t InvariantMass(Double_t m_b, Double_t m_t, Double_t T_b) { return 2*m_t*T_b + pow(m_b + m_t,2); }
+  Double_t MaxTheta_ej(Double_t p_CM, Double_t rapidity, Double_t m_ej) { return asin(p_CM / (m_ej*rapidity)); }
+  Double_t MaxTheta_rec(Double_t p_CM, Double_t rapidity, Double_t m_rec) { return asin(p_CM / (m_rec*rapidity)); }
+  Double_t p_CM(Double_t m_b, Double_t m_t, Double_t S);
+  Double_t Rapidity(Double_t p_CM, Double_t m_t) { return log( (p_CM + sqrt(m_b*m_b + p_CM*p_CM)) / m_b); }
+  Double_t p_ej(Double_t p_CM, Double_t rapidity, Double_t theta_CM, Double_t m_ej);
+  Double_t p_rec(Double_t p_CM, Double_t rapidity, Double_t theta_CM, Double_t m_rec);
+  Double_t T_ej(Double_t p_ej, Double_t m_ej) { return sqrt(p_ej*p_ej + m_ej*m_ej ) - m_ej; }
+  Double_t T_rec(Double_t p_rec, Double_t m_rec) { return sqrt(p_rec*p_rec + m_rec*m_rec ) - m_rec; }
+  Double_t ThetaCM_ej(Double_t p_CM, Double_t p_ej, Double_t theta_Lab) { return TMath::2*Pi()*asin( sin(theta_rad)*p_ej / p_CM); }
+  Double_t ThetaCM_rec(Double_t theta_Lab) { return TMath::Pi() - theta_ej; }
+
+
+private: // private data members
+  TAtomicMassTable *fAMT;              //!
+  TAtomicMassTable::MassExcess_t *fME; //!
+  TAtomicMassTable::Nucleus_t *fNuc;   //!
+
+private: // private member functions
+  Double_t CalcMassEnergy(Int_t A, Double_t Delta){ return A*Constants.AMU() / 1.0e3 + Delta; }
   Double_t GetExcess(Int_t Z, Int_t A);
-  Double_t CalcMassEnergy(Int_t A, Double_t Delta);
-  Double_t ClacS(Double_t m_b, Double_t m_t, Double_t E_b);
-  Double_t CalcXi0(Double_t m_b, Double_t m_t);
-  Double_t CalcXi1(Double_t m_b, Double_t m_t, Double_t E_b);
+  void CalcVars();
+  void reset();
+  void SetVars();
+
 
 private:
   ClassDef(sonik::Kinematics,1)
--- src/Kinematics.cxx
+++ src/Kinematics.cxx
@@ -5,51 +5,165 @@
 //////////////////////////////////////////////////////
 
 #include "Kinematics.hxx"
+
 #include "TAtomicMass.h"
 #include "Constants.hxx"
+using namespace dragon;
+
+#include "TMath.h"
+
 
 void sonik::Kinematics::Kinematics();
 {
-  TAtomicMassTable *TAMT = new TAtomicMassTable();
+  TAtomicMassTable *fAMT = new TAtomicMassTable();
   reset();
 }
 
-void sonik::Kinematics::Kinematics(Int_t Z_b, Int_t A_b, Int_t Z_t, Int_t A_t, Double_t E_b, Double_t theta)
+
+void sonik::Kinematics::Kinematics(Int_t Z_b, Int_t A_b, Int_t Z_t, Int_t A_t, Double_t T_b, Double_t theta_Lab)
 {
-  TAtomicMassTable *TAMT = new TAtomicMassTable();
-  TAtomicMassTable::MassExcess_t *Delta_b = TAMT.MassExcess_t;
-  TAtomicMassTable::MassExcess_t *Delta_t = TAMT.MassExcess_t;
-  Delta_b = Delta_b->GetMassExcess(Z_b, A_b);
-  Delta_t = Delta_t->GetMassExcess(Z_t, A_t);
-  RxnPair.fDelta_b = Delta_b.fValue;
-  RxnPair.fDelta_t = Delta_t.fValue;
+  TAtomicMassTable *fAMT = new TAtomicMassTable();
+  TAtomicMassTable::MassExcess_t *fME;
+  SetVars(Z_b, A_b, Z_t, A_t, T_b);
 }
 
-void sonik::Kinematics::Kinematics(const char Symbol_b, const char Symbol_t, Double_t E_b)
-{
 
+void sonik::Kinematics::Kinematics(const char Symbol_b, const char Symbol_t, Double_t T_b)
+{
+  TAtomicMassTable *fAMT = new TAtomicMassTable();
+  TAtomicMassTable::MassExcess_t *fME;
+  TAtomicMassTable::Nucleus_t *fNuc;
+  SetVars(Symbol_b, Symbol_t, T_b);
 }
 
 void sonik::Kinematics::reset()
 {
-  RxnPair.fSymbol_b   = 0;
-  RxnPair.fSymbol_t   = 0;
-  RxnPair.fA_b        = 0;
-  RxnPair.fA_t        = 0;
-  RxnPair.fZ_b        = 0;
-  RxnPair.fZ_t        = 0;
-  RxnPair.fDelta_b    = 0;
-  RxnPair.fDelta_t    = 0;
-  RxnPair.fm_b        = 0;
-  RxnPair.fm_t        = 0;
-  RxnPair.fej_b       = 0;
-  RxnPair.frec_t      = 0;
-  RxnPair.fE_b        = 0;
-  RxnPair.fGammaLab   = 0;
-  RxnPair.fBetaLab    = 0;
-  RxnPair.fminvariant = 0;
-  RxnPair.fRapidity   = 0;
-  RxnPair.fTheta_CM   = 0;
-  RxnPair.fT_ej       = 0;
-  RxnPair.fT_rec      = 0;
+  Rxn_t.fSymbol_b   = 0;
+  Rxn_t.fSymbol_t   = 0;
+  Rxn_t.fA_b        = 0;
+  Rxn_t.fA_t        = 0;
+  Rxn_t.fZ_b        = 0;
+  Rxn_t.fZ_t        = 0;
+  Rxn_t.fDelta_b    = 0;
+  Rxn_t.fDelta_t    = 0;
+  Rxn_t.fm_b        = 0;
+  Rxn_t.fm_t        = 0;
+  Rxn_t.fej_b       = 0;
+  Rxn_t.frec_t      = 0;
+  Rxn_t.fE_b        = 0;
+  Rxn_t.fGammaLab   = 0;
+  Rxn_t.fBetaLab    = 0;
+  Rxn_t.fminvariant = 0;
+  Rxn_t.fRapidity   = 0;
+  Rxn_t.fTheta_CM   = 0;
+  Rxn_t.fT_ej       = 0;
+  Rxn_t.fT_rec      = 0;
+}
+
+
+Double_t p_CM(Double_t m_b, Double_t m_t, Double_t s)
+{
+  return sqrt((pow(s - m_b*m_b - m_t*m_t,2) - 4*m_t*m_t*m_b*m_b) / (4*s) );
+}
+
+
+Double_t p_ej(Double_t p_CM, Double_t rapidity, Double_t theta_rad, Double_t m_ej)
+{
+  return ( sqrt(m_ej*m_ej + p_CM*p_CM)*cos(theta_rad)*sinh(rapidity)+cosh(rapidity)*
+           sqrt(p_CM*p_CM - pow(m_ej*sin(thet_rad)*sinh(rapidity),2) ) ) /
+    ( 1 + pow(sin(theta_rad)*sinh(rapidity),2) );
+}
+
+
+Double_t p_rec(Double_t p_CM, Double_t rapidity, Double_t theta_CM, Double_t m_rec)
+{
+  return ( sqrt(m_rec*m_rec + p_CM*p_CM)*cos(theta_rad)*sinh(rapidity)+cosh(rapidity)*
+           sqrt(p_CM*p_CM - pow(m_rec*sin(thet_rad)*sinh(rapidity),2) ) ) /
+    ( 1 + pow(sin(theta_rad)*sinh(rapidity),2) );
+}
+
+
+Double_t GetExcess(Int_t Z, Int_t A)
+{
+  TAtomicMassTable::MassExcess_t *Delta;
+  Delta = fAMT->GetMassExcess(Z, A);
+  return Delta.fValue / 1.0e3;
+}
+
+
+Rxn_t* ScatteredVars(Int_t Z_b, Int_t A_b, Int_t Z_t, Int_t A_t, Double_t T_b, Double_t theta_Lab)
+{
+  Rxn_t.fGammaLab    = GammaLab(Rxn_t.fm_b, T_b);
+  Rxn_t.fBetaLab     = BetaLab(Rxn_t.fGamaLab);
+  Rxn_t.fInvariantM  = InvariantMass(Rxn_t.fm_b, Rxn_t.fm_t, T_b);
+  Rxn_t.fp_CM        = p_CM(Rxn_t.fm_b, Rxn_t.fm_t, Rxn_t.InvariantM);
+  Rxn_t.fRapidity    = Rapidity(Rxn_t.fp_CM, Rxn_t.fm_t);
+  Rxn_t.fGammaCM     = GammaLab(Rxn_t.fRapidity);
+  Rxn_t.fBetaLab     = BetaLab(Rxn_t.fRapidity);
+  Rxn_t.fp_ej        = p_ej(p_CM, rapidity, theta_CM, m_ej);
+  Rxn_t.fp_rec       = p_rec(p_CM, rapidity, theta_CM, m_rec);
+  Rxn_t.fThetaCM_ej  = ThetaCM_ej(p_CM, p_ej, theta_Lab);
+  Rxn_t.fThetaCM_rec = ThetaCM_rec(theta_Lab);
+}
+
+
+void SetVars(Int_t Z_b, Int_t A_b, Int_t Z_t, Int_t A_t, Double_t T_b)
+{
+  Rxn_t.fT_b        = T_b;
+  Rxn_t.fVelSquared = T_b / Rxn_t.fm_b;
+  Rxn_t.fTheta_Lab  = theta_Lab;
+  Rxn_t.fA_b        = A_b;
+  Rxn_t.fA_t        = A_t;
+  Rxn_t.fZ_b        = Z_b;
+  Rxn_t.fZ_t        = Z_t;
+  Rxn_t.fDelta_b    = GetExcess(Z_b, A_b);
+  Rxn_t.fDelta_t    = GetExcess(Z_t, A_t);
+  Rxn_t.fm_b        = MassEnergy(A_b, Rxn_t.fDelta_b);
+  Rxn_t.fm_t        = MassEnergy(A_t, Rxn_t.fDelta_t);
+
+
+  if ( A_t == A_b && Z_t == Z_b ){
+    cout << "Error: Mott scattering is not supported yet! \n";
+    exit (EXIT_FAILURE);
+  }
+  else if( Rxn_t.fm_t < Rxn_t.fm_b ){
+    Rxn_t.fm_ej  = Rxn_t.fm_t;
+    Rxn_t.fm_rec = Rxn_t.fm_b;
+  }
+  else{
+    Rxn_t.fm_ej  = Rxn_t.fm_b;
+    Rxn_t.fm_rec = Rxn_t.fm_t;
+  }
+
+}
+
+
+void SetVars(const char Symbol_b, const char Symbol_t, Double_t T_b)
+{
+  Rxn_t.fT_b        = T_b;
+  Rxn_t.fVelSquared = T_b / Rxn_t.fm_b;
+  Rxn_t.fTheta_Lab  = theta_Lab;
+  Rxn_t.fA_b        = A_b;
+  Rxn_t.fA_t        = A_t;
+  Rxn_t.fZ_b        = Z_b;
+  Rxn_t.fZ_t        = Z_t;
+  Rxn_t.fDelta_b    = ME->GetExcess(Symbol_b);
+  Rxn_t.fDelta_t    = ME->GetExcess(Symbol_t);
+  Rxn_t.fm_b        = MassEnergy(A_b, Rxn_t.fDelta_b);
+  Rxn_t.fm_t        = MassEnergy(A_t, Rxn_t.fDelta_t);
+
+
+  if ( A_t == A_b && Z_t == Z_b ){
+    cout << "Error: Mott scattering is not supported yet! \n";
+    exit (EXIT_FAILURE);
+  }
+  else if( Rxn_t.fm_t < Rxn_t.fm_b ){
+    Rxn_t.fm_ej  = Rxn_t.fm_t;
+    Rxn_t.fm_rec = Rxn_t.fm_b;
+  }
+  else{
+    Rxn_t.fm_ej  = Rxn_t.fm_b;
+    Rxn_t.fm_rec = Rxn_t.fm_t;
+  }
+
 }
